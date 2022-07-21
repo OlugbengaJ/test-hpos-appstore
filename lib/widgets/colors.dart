94942b2a9be7ec1597ac1c9e8d79af73dec51832
Favour Chukwuemeka <chikeluba.chukwuemeka@andela.com>
2022-07-21 03:07:35 -0700

    Updated layout

--- lib/widgets/colors.dart
+++ lib/widgets/colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color primary = const Color(0xff434afa);
+  static Color primary = const Color(0xff4758F6);
   static Color secondary = const Color(0xff434afa);
   static Color dark = const Color(0xff434afa);
-  static Color white = const Color(0xff434afa);
+  static Color white = const Color(0xffffffff);
   static Color tertiary = const Color(0xff434afa);
   static Color danger = const Color(0xFFFA4343);
   static Color success = const Color(0xff434afa);
--- lib/widgets/layouts/app_layout.dart
+++ lib/widgets/layouts/app_layout.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hpos_appstore/widgets/colors.dart';
+import 'package:hpos_appstore/widgets/layouts/sidebar_layout.dart';
 
 class AppLayout extends StatefulWidget {
   const AppLayout({
     Key? key,
-    required this.left,
     this.leftFlex = 1,
     required this.right,
     this.rightFlex = 8,
   }) : super(key: key);
-  final Widget left;
   final int leftFlex;
   final Widget right;
   final int rightFlex;
@@ -22,36 +21,48 @@ class _AppLayoutState extends State<AppLayout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
+        body: Container(
+      decoration: const BoxDecoration(
+        color: Color(0xff4758F6),
+      ),
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           SizedBox(
-              width: 100,
+              width: 111,
               child: Container(
-                  alignment: AlignmentDirectional.bottomCenter,
-                  padding: const EdgeInsets.only(bottom: 50.0),
-                  color: AppColors.primary,
-                  child: widget.left)),
+                  color: AppColors.primary, child: const SidebarLayout())),
           Expanded(
-            child: Column(
-              children: [
-                AppBar(
-                  bottomOpacity: 0.0,
-                  elevation: 0.0,
-                ),
-                Row(
+            child: Container(
+              margin: const EdgeInsets.only(top: 16),
+              decoration: const BoxDecoration(
+                  color: Color(0xffffffff),
+                  borderRadius:
+                      BorderRadius.only(topLeft: Radius.circular(40))),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10, left: 30, right: 10),
+                child: Column(
                   children: [
-                    Expanded(
-                      flex: widget.rightFlex,
-                      child: widget.right,
+                    AppBar(
+                      backgroundColor: AppColors.dark,
+                      bottomOpacity: 0.0,
+                      elevation: 0.0,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: widget.rightFlex,
+                          child: widget.right,
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
           )
         ],
       ),
-    );
+    ));
   }
 }
