1e471af5e86f303eb256f174cad1a9d5d6493a69
Favour Chukwuemeka <chikeluba.chukwuemeka@andela.com>
2022-07-21 03:07:35 -0700

    Updated layout

--- lib/screens/home_screen/homes_screen.dart
+++ lib/screens/home_screen/homes_screen.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/widgets.dart';
 import 'package:hpos_appstore/widgets/layouts/app_layout.dart';
-import 'package:hpos_appstore/widgets/layouts/sidebar_layout.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AppLayout(
       right: Container(),
-      left: const SidebarLayout(),
     );
   }
 }
--- lib/screens/library_screens/library_screen.dart
+++ lib/screens/library_screens/library_screen.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/widgets.dart';
 import 'package:hpos_appstore/widgets/layouts/app_layout.dart';
-import 'package:hpos_appstore/widgets/layouts/sidebar_layout.dart';
 
-class LibraryScreen extends StatefulWidget {
-  const LibraryScreen({Key? key}) : super(key: key);
-
-  @override
-  State<LibraryScreen> createState() => _LibraryScreenState();
-}
-
-class _LibraryScreenState extends State<LibraryScreen> {
+class LibraryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AppLayout(
       right: Container(),
-      left: const SidebarLayout(),
     );
   }
 }
