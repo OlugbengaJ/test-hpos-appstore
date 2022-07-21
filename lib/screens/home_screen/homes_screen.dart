0da342df96d680b6abac6ce72de4fc6198e725ff
Favour Chukwuemeka <chikeluba.chukwuemeka@andela.com>
2022-07-21 06:37:23 -0700

    added const head

--- lib/screens/home_screen/homes_screen.dart
+++ lib/screens/home_screen/homes_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/widgets.dart';
 import 'package:hpos_appstore/widgets/layouts/app_layout.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return AppLayout(
--- lib/screens/library_screens/library_screen.dart
+++ lib/screens/library_screens/library_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/widgets.dart';
 import 'package:hpos_appstore/widgets/layouts/app_layout.dart';
 
 class LibraryScreen extends StatelessWidget {
+  const LibraryScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return AppLayout(
