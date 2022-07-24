6d28e0141c1a5dd71b23fe6e3b1a549e4848f1a8
adelekeoadelaja <adeleke.adelaja@andela.com>
2022-07-24 19:33:32 +0300

    init searchbar component

---
+++ lib/widgets/components/searchbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Searchbar extends StatelessWidget {
+  const Searchbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}
