59c9caef48aa9cce4f5778189ed434460148cc1b
Philemon Gumah <gumah196@gmail.com>
2022-08-02 10:14:49 +0000

    fixes
    - library buttons
    - library header
    - touched app layout to fix app header
    - touched card product list item to fix arrangement

--- lib/widgets/components/product_card/button_update_delete.dart
+++ lib/widgets/components/product_card/button_update_delete.dart
@@ -21,90 +21,90 @@ class ButtonUpdateDelete extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 10),
-      child: Wrap(
-        children: [
-          SizedBox(
-            height: 35.0,
-            width: 100,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 0.0,
-                primary: (hasUpdate == true)
-                    ? AppColors.primaryW500
-                    : AppColors.primaryW400,
-                shape: const RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.all(Radius.circular(Numericals.double8)),
-                ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      // crossAxisAlignment:WrapCrossAlignment.center,
+      // alignment: WrapAlignment.end,
+      children: [
+        SizedBox(
+          height: 35.0,
+          width: 100,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              elevation: 0.0,
+              primary: (hasUpdate == true)
+                  ? AppColors.primaryW500
+                  : AppColors.primaryW400,
+              shape: const RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.all(Radius.circular(Numericals.double8)),
               ),
-              onPressed: () => {},
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    (hasUpdate == true) ? AppTexts.update : AppTexts.installed,
-                    style: const TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
+            ),
+            onPressed: () => {},
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  (hasUpdate == true) ? AppTexts.update : AppTexts.installed,
+                  style: const TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const Padding(padding: EdgeInsets.only(left: 16)),
+        SizedBox(
+          height: 35.0,
+          // width: 130,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              side: const BorderSide(
+                width: 1.0,
+                color: AppColors.red,
+              ),
+              elevation: 0.0,
+              primary: AppColors.white,
+              shape: const RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.all(Radius.circular(Numericals.double8)),
+              ),
+            ),
+            onPressed: () => {},
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Wrap(
+                  children: [
+                    const Text(
+                      AppTexts.uninstall,
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.red,
+                      ),
                     ),
-                  ),
-                ],
-              ),
+                    const Padding(padding: EdgeInsets.only(left: 10)),
+                    InkWell(
+                      onTap: () {},
+                      child: const Icon(
+                        Icons.delete_outline,
+                        size: 20,
+                        color: AppColors.red,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-          const Padding(padding: EdgeInsets.only(left: 5)),
-          SizedBox(
-            height: 35.0,
-            width: 130,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                side: const BorderSide(
-                  width: 1.0,
-                  color: AppColors.red,
-                ),
-                elevation: 0.0,
-                primary: AppColors.white,
-                shape: const RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.all(Radius.circular(Numericals.double8)),
-                ),
-              ),
-              onPressed: () => {},
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Wrap(
-                    children: [
-                      const Text(
-                        AppTexts.uninstall,
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w500,
-                          color: AppColors.red,
-                        ),
-                      ),
-                      const Padding(padding: EdgeInsets.only(left: 10)),
-                      InkWell(
-                        onTap: () {},
-                        child: const Icon(
-                          Icons.delete_outline,
-                          size: 20,
-                          color: AppColors.red,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
